56a624565072d1310c999f2996932fb91773d25c
JoaoFernandoRangel <122740076+JoaoFernandoRangel@users.noreply.github.com>
2023-03-04 18:05:05 -0300

    Add files via upload

--- Lista_1.m
+++ Lista_1.m
@@ -1,27 +1,50 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Aluno: João Fernando Rangel
 %Rascunho para resolução da primeira lista de exercícios
-%Aplica degrau unitário a uma funcao
+%Rascunho da lista 1
 %Data de criação: 04/03/2023  Ultima modificação: 04/03/2023
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 close all
 clear all
-t = -10:1:10;
+t = -10:0.01:10;
 tamanho = length(t);
 saida = 0:0:100;
-x = 0:0:100;
+%funcao = t*(heaviside(t)-heaviside(t-1));
 
 
 %degrau 1
 for i=1:1:tamanho
-    %x(i) = t(i)*(heaviside(t(i)-heaviside(t(i)-1)));%funcao(i)=x(t)
-    funcao_mais_um(i) = (t(i)+1)*(heaviside(t(i)+1) - heaviside(t(i)));
-    funcao_menos_um(i) = (1-t(i))*(heaviside(1-t(i))-heaviside(-t(i)));
-    saida(i) = funcao_mais_um(i) + funcao_menos_um(i);
-end
+    argumento1(i)=(t(i)+2)/4;
+    argumento2(i)=(2-t(i))/4;
+    argumento3(i)=(t(i)/2);
+    argumento4(i)=(-t(i)/2);
+    funcao1(i) = argumento1(i)*(heaviside(argumento1(i))-heaviside(argumento1(i)-1));
+    funcao2(i) = argumento2(i)*(heaviside(argumento2(i))-heaviside(argumento2(i)-1));
+    funcao3(i) = argumento3(i)*(heaviside(argumento3(i))-heaviside(argumento3(i)-1));
+    funcao4(i) = argumento4(i)*(heaviside(argumento4(i))-heaviside(argumento4(i)-1));
 
-%axis([(t(1)-1) (t(tamanho)+1) (saida(1)-1) (saida(tamanho)+1)]) 
-figure(1)
-plot(t,saida)
-xlim([t(1)-1 t(tamanho)+1])
-ylim([(saida(1)-1) (saida(tamanho)+1)])
+    saida(i) = funcao1(i) + funcao2(i)+funcao3(i)+funcao4(i);
+end
+%
+%%
+
+for j=1:1:tamanho
+    argumento(j) = ((1/3)*(-t(j))+(1/3)); 
+    plotando(j) = 0.5*argumento(j)*(heaviside(-argumento(j)-1) - heaviside(-argumento(j)+1));
+end
+plot(t,plotando)
+hold on
+%plot(t, heaviside(-t(:)-1),'r')
+hold on
+%plot(t,saida, 'b')
+%plot(t,saida)
+
+
+
+
+
+
+
+
+
+
